efffb0f8b81d656ef67dcadae004843cc1cccbd2
YWSIK <65442256+YWSIK@users.noreply.github.com>
2023-04-06 17:02:48 +0900

    Update 평균.py

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,9 +1,12 @@
-subject = int(input())  # 과목 수
+#1546
+# 점수 중 최댓값 M, 모든점수를 점수 / M * 100 으로
+
+subject = int(input())  # 과목수
 score = list(map(int, input().split()))
 max_score = max(score)
 
 new_score = []
 for s in score :
-    new_score.append(s / max_score *100)  # 새로운 점수 생성
+    new_score.append(s / max_score *100)  # 새로운 점수
 avg = sum(new_score) / subject
 print(avg)
