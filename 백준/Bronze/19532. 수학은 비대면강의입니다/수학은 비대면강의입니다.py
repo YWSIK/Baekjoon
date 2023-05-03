c5f008d69d1b8c659aec78ba829c9181b7bf73d6
YWSIK <65442256+YWSIK@users.noreply.github.com>
2023-05-03 16:53:41 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 696 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,12 @@
+import sys
+
+a, b, c, d, e, f = map(int, sys.stdin.readline().split())
+
+result = []
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            result = [x, y]
+            break
+        
+print(result[0] , result[1])
