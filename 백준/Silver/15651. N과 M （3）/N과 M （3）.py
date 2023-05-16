7a6e79e2b4a1adafbeb4327ed2f0a1d6ecc35248
YWSIK <65442256+YWSIK@users.noreply.github.com>
2023-05-16 13:05:27 +0900

    [Silver III] Title: N과 M (3), Time: 1392 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,15 @@
+#15651
+n, m = map(int, input().split())
+arr = []
+
+def back():
+    if len(arr) == m:
+        print(" ".join(map(str, arr)))
+        return
+    
+    for i in range(1, n+1):
+        arr.append(i)
+        back()
+        arr.pop()
+        
+back()
