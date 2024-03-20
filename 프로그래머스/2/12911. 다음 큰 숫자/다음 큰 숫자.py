e4dd1283e3bf130bc476da797a18ed1e2307b97b
SIK <65442256+YWSIK@users.noreply.github.com>
2024-03-21 02:09:16 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,11 +1,9 @@
 def solution(n):
 
-    bin_n = bin(n)[2:]
-    cnt = bin_n.count('1')
+    n_cnt = bin(n)[2:].count('1')
     
     for i in range(n+1, 2*n+1):
-        bin_i = bin(i)[2:]
-        next_cnt = bin_i.count('1')
+        next_cnt = bin(i)[2:].count('1')
         
-        if cnt == next_cnt:
+        if n_cnt == next_cnt:
             return i
